62fde4921547667b9f378e2e73bbdd12327d1a6c
Andrew Kemp <andrewkemp70@gmail.com>
2013-10-19 13:15:58 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theorientationexpress::Application.config.secret_token = 'b556dbfe32b0b3ff093d5894c1a03555c527f063bb1d027e85f338de63f5af24f820df8ffc4ff7ca15ced1d92efd180dda80f326acb29d5e8bc4b69af9941588'
